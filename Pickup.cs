92e4ee1a4cdce561ed0bff648661839a5fbf8d44
sandeepbalwada-hub <sambalwada1402@gmail.com>
2025-11-09 18:08:47 +0530

    Update Pickup.cs

--- Pickup.cs
+++ Pickup.cs
@@ -10,15 +10,3 @@ public class Pickup : MonoBehaviour
     }
 }
 
-Obstacle.cs
-
-using UnityEngine;
-
-public class Obstacle : MonoBehaviour
-{
-    void OnTriggerEnter(Collider other)
-    {
-        if (!other.CompareTag("Player")) return;
-        FindFirstObjectByType<UIManager>().ShowGameOver();
-    }
-}
